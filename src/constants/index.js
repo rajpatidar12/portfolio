1baa729f743aef7f1e97c74b8be60fa7441f81fb
rajpatidar12 <patidarraj465@gmail.com>
2024-11-29 16:42:12 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,134 @@
+import {
+  backend,
+  creator,
+  web,
+  javascript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  git,
+  linkedIn,
+  github,
+} from "../assets";
+
+const navigationPaths = {
+  home: "/",
+  about: "about",
+  work: "work",
+  contact: "contact",
+};
+
+export const navLinks = [
+  {
+    id: navigationPaths.about,
+    title: "About",
+  },
+  {
+    id: navigationPaths.work,
+    title: "Work",
+  },
+  {
+    id: navigationPaths.contact,
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "React Developer",
+    icon: web,
+  },
+  {
+    title: "NodeJs Developer",
+    icon: backend,
+  },
+  {
+    title: "Javascript Developer",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "Git",
+    icon: git,
+  },
+];
+
+const experiences = [];
+
+const projects = [];
+
+const personalInfo = {
+  name: "Raj",
+  fullName: "Patidar",
+  email: "Patidarraj465@gmail.com",
+  role: "Software Developer",
+  about: `I'm a skilled software developer with 
+  JavaScript, and expertise in frameworks like React.Js, React-naitve,
+  Node.js etc. I'm a quick learner and collaborate closely with clients to
+  create efficient, scalable, and user-friendly solutions that solve
+  real-world problems. Let's work together to bring your ideas to life!`,
+  projectsIntro: `Following projects showcases my skills and experience through
+  real-world examples of my work. Each project is briefly described with
+  live demos. It reflects my ability to solve complex problems, work
+  with different technologies, and manage projects effectively.`,
+};
+
+const publicUrls = {
+  resume:
+    "https://github.com/rajpatidar12/resume/blob/main/Raj%20Patidar%20(1).pdf",
+  socialProfiles: {
+    linkedin: {
+      title: "linkedin",
+      link: "https://www.linkedin.com/in/rajpatidar1/",
+      icon: linkedIn,
+    },
+    github: {
+      title: "github",
+      link: "https://github.com/rajpatidar12",
+      icon: github,
+    },
+  },
+};
+
+export {
+  services,
+  technologies,
+  experiences,
+  projects,
+  navigationPaths,
+  personalInfo,
+  publicUrls,
+};
